94c1faf646aea130c604d5fe2881d76be2fc4f74
Benjamin Brandmeier <b.brandmeier@fme.de>
2017-12-19 20:40:31 +0100

    format

--- process_test.go
+++ process_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestKillProcess(t *testing.T) {
-	sleep := exec.Command("sleep","5")
+	sleep := exec.Command("sleep", "5")
 	sleep.Start()
-	
+
 	err := KillProcess("sleep.exe")
 	if err != nil {
 		t.Error("Killing process failed:", err)
